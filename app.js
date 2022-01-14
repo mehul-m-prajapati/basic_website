b4bf58ac0975d0bd0c4501bfb542dcb42388d8e5
Mehul <mehulprajapati2802@gmail.com>
2022-01-13 23:45:05 -0500

    mdn assessment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 7000;
 app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/assets/'));
 
 
 app.get('/', (req, res) => {
